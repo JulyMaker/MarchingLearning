d4468324cf14545a3f30b24e0051f062580d1748
JulioUrjc <julio.martin.saez@gmail.com>
2020-06-26 09:33:59 +0200

    Random forest finished

--- 04RandomForest.py
+++ 04RandomForest.py
@@ -5,7 +5,7 @@ import seaborn as sns
 
 #TrainModel
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 #TestModel
 from sklearn.metrics import classification_report, confusion_matrix
@@ -15,17 +15,16 @@ vinos = pd.read_csv('./resources/vinos.csv')
 print(vinos.columns)
 
 x = vinos.drop('Wine Type', axis=1)
-
-y= vinos['Wine Type']
+y = vinos['Wine Type']
 
 # Train Model #
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=45)
 
-arbol = DecisionTreeClassifier()
-arbol.fit(x_train, y_train)
+randomforest = RandomForestClassifier(n_estimators=80)
+randomforest.fit(x_train, y_train)
 
 # Test Model #
-predicciones = arbol.predict(x_test)
+predicciones = randomforest.predict(x_test)
 
 print(classification_report(y_test, predicciones))
 print(confusion_matrix(y_test, predicciones))
