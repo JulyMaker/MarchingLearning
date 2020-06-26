68a75d8cae941c1844ceeb412600b33b468c7f62
JulioUrjc <julio.martin.saez@gmail.com>
2020-06-26 02:15:00 +0200

    potly express found

--- ejemplos/ejemplosPlotly.py
+++ ejemplos/ejemplosPlotly.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import numpy as np
-import cufflinks as cf
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+#import cufflinks as cf
+#from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import matplotlib.pyplot as plt
+import plotly.express as px
 
-init_notebook_mode(connected=True)
-
-cf.go_offline()
-
+#init_notebook_mode(connected=True)
+#cf.go_offline()
 #%matplotlib inline
 
 dataframe = pd.DataFrame(np.random.randn(100,4), columns=['a','b','c','d'])
@@ -15,7 +14,8 @@ dataframe = pd.DataFrame(np.random.randn(100,4), columns=['a','b','c','d'])
 #plt.show()
 
 #dataframe.iplot()
-dataframe.iplot(kind='box')
+#dataframe.iplot(kind='box')
 #dataframe.iplot(kind='surface')
 #dataframe['a'].iplot(kind='hist', bins=30)
-plt.show()
+fig = px.histogram(dataframe, x="a", nbins=30)
+fig.show()
